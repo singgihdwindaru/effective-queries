d58c919d19e90d0c14f4b633c558e93ab536f34c
crowninja30 <crowgithub30@gmail.com>
2021-06-25 02:02:42 +0700

    init project

---
+++ Application.Api/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Application.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        
+
+    }
+}
